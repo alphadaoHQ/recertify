ee743f681dec7816b64903794eea80345d6d4a73
justified4636 <imohimijusticeafesuan@gmail.com>
2026-01-16 00:22:20 +0000

    provider.tsx

--- certification-nft-dapp/src/app/providers.tsx
+++ certification-nft-dapp/src/app/providers.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client';
 
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import { initTelegramWebApp } from "@/lib/telegram";
-import type { ReactNode } from "react";
-import { useEffect } from "react";
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { initTelegramWebApp } from '@/lib/telegram';
+import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 
 const _manifestUrl = `${process.env.NEXT_PUBLIC_APP_URL}/tonconnect-manifest.json`;
 
 export function Providers({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    // Initialize Telegram Web App
-    initTelegramWebApp();
-  }, []);
+    useEffect(() => {
+        // Initialize Telegram Web App
+        initTelegramWebApp();
+    }, []);
 
-  return (
-    <TonConnectUIProvider manifestUrl="https://peach-fast-clam-38.mypinata.cloud/ipfs/bafkreidsqkapogy6yric4zskh76r5ldsdrstwrlnvsidb2fzi2tflqzywa">
-      {children}
-    </TonConnectUIProvider>
-  );
+    return <TonConnectUIProvider manifestUrl={_manifestUrl}>{children}</TonConnectUIProvider>;
 }
